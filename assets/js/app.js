1abe8372dd85c0825cb065bc87c5a7d9e73b12e2
Brandon Heck <bhecky99@gmail.com>
2019-03-28 15:21:21 -0400

    Had made changes but then I undid them. Commiting since I had did changes with then undoing those changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,74 +1,73 @@
-//JavaScript Document
-
+// JavaScript Document
 
 window.onload = init();
 
 function init() {
-  window.addEventListener('scroll', function(e) {
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 300,
-      header = document.querySelector("header");
-    if (distanceY > shrinkOn) {
-      classie.add(header, "smaller");
-    } else {
-      if (classie.has(header, "smaller")) {
-        classie.remove(header, "smaller");
-      }
-    }
-  });
+
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header, "smaller");
+        } else {
+            if (classie.has(header, "smaller")) {
+                classie.remove(header, "smaller");
+            }
+        }
+    });
 
 
-  $.ajax({
+    $.ajax({
+        method: 'GET',
+        url: 'assets/data/menu.json',
+        dataType: 'json',
+        success: function (data) {
 
-    method: 'GET',
-    url: 'assets/data/menu.json',
-    dataType: 'json',
-    success: function(data) {
+            var menu = menuBuilder(data.menu);
 
-      var menu = menuBuilder(data.menu);
+            $('nav').append(menu);
 
-      $('nav').append(menu);
+        },
+        error: function () {
 
-    },
+            console.log('all is not good');
 
-    error: function() {
-      console.log('all is not good');
-    }
-  });
-
+        }
+    });
 
 }
 
 
-
 function menuBuilder(obj) {
 
-  var theMenu = '';
+    var theMenu = '';
 
-  if (obj.length > 0) {
+    if (obj.length > 0) {
 
+        theMenu = theMenu + '<ul>';
 
-    theMenu = theMenu + '<ul>';
-    obj.forEach(function(item) {
+        obj.forEach(function (item) {
 
-      theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+            theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
 
-      if (item.Menus.length > 0) {
+            if (item.Menus.length > 0) {
 
-        theMenu = theMenu + menuBuilder(item.Menus);
-      };
+                theMenu = theMenu + menuBuilder(item.Menus);
 
-      theMenu = theMenu + '</li>';
+            }
 
-    });
+            theMenu = theMenu + '</li>';
 
-    theMenu = theMenu + '</ul>';
+        });
 
-  } else {
-    console.log('no data');
+        theMenu = theMenu + '</ul>';
 
-  }
+    } else {
 
-  return theMenu;
+        console.log('no data');
 
+    }
+
+    return theMenu;
 }
